8859262dd3ce9084a6cf44a6b3bfd396a84a87a5
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-04-05 14:46:18 +0300

    lesson for using read and write, synchronously and asynchronously

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,2 +1,23 @@
-const hello = "Hello world";
-console.log(hello);
+const fs = require('fs');
+
+//Blocking, synchronous way
+/*const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+const textOut = `This is what we know about the ovacado: ${textIn}.\nCreatead on ${Date.now()}`;
+fs.writeFileSync('./txt/output2.txt', textOut);
+console.log('File written');*/
+
+//Non Blocking, Synchronous way
+fs.readFile('./txt/start.txt', 'utf-8',    (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8',    (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8',    (err, data3) => {
+            console.log(data3);
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your File has been written');
+            });
+        });
+    });
+});
+
+console.log('Will read file');
