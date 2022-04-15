ef8ad71a9ea747a45c1b3e7c972db9251462c053
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-04-15 07:30:30 +0300

    updates index.js

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -57,19 +57,57 @@ console.log('Will read file');*/
 //         res.end('<h1>This page could not be found</h1>');
 //     }
 // });
+
+//Templates
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+//Data
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+//Replaces template
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    }
+
+    return output;
+};
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
     const pathName = req.url;
+
+    //Overview page
     if (pathName === '/' || pathName === 'overview') {
-        res.end('This is an overview');
+        res.writeHead(200,{'Content-type': 'text/html'});
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        console.log(cardsHtml)
+
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+
+    //Product Page
     }else if (pathName === 'product') {
         res.end('This is a PRODUCT');
+
+    //API
     }else if (pathName === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
+
+    //NOT FOUND
     } else {
         res.writeHead('404', {
             'Content-type': 'text/html',
