d19406b1e83f3b9e17a075cf5b985cba10aa09b0
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-04-18 08:02:41 +0300

    adds package.json, .gitignore

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,11 @@
+//Native module imports
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+//Custom module imports
+const replaceTemplate = require('./modules/replaceTemplate');
+
 ////////////////////////////////////////////////////////////
 /////// Files
 
@@ -67,24 +72,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-//Replaces template
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
 
-    if (product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-
-    return output;
-};
-
+//Creates nodejs server
 const server = http.createServer((req, res) => {
    const {query, pathname} = url.parse(req.url, true);
 
@@ -92,29 +81,32 @@ const server = http.createServer((req, res) => {
     if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200,{'Content-type': 'text/html'});
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        //console.log(cardsHtml)
-
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
 
     //Product Page
     }else if (pathname === '/product') {
+
         res.writeHead(200, {'Content-type': 'text/html'});
         const product = dataObj[query.id];
         output = replaceTemplate(tempProduct, product)
         res.end(output);
+
     //API
     }else if (pathname === '/api') {
+
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
 
     //NOT FOUND
     } else {
+
         res.writeHead('404', {
             'Content-type': 'text/html',
             'my-custom-header': 'Hello World'
         });
         res.end('Page not found');
+
     }
 });
 
