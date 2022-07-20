3078611427dc02c4237044f54c3243760bac0f95
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-07-21 01:45:47 +0300

    adds get and post routes for tours

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,23 +1,85 @@
+const fs = require('fs');
 const express = require('express');
+
 const app = express()
 
-const PORT = 3000;
+app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res
-        .status(200)
-        .json({
-            message:'Hello from server side!',
-            app: 'Natours'
-        });
+//
+// app.get('/', (req,res)=>{
+//     res
+//         .status(200)
+//         .json({
+//             message:'Hello from server side!',
+//             app: 'Natours'
+//         });
+//
+//     console.log(res);
+// });
+//
+// app.post('/', (req,res) => {
+//     res.send('You can post to this endpoint....');
+// });
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+//fs.writeSync(`${__dirname}/dev-data/data/tours-simple.json`);
+
+app.get('/api/v1/tours', (req,res)=>{
+    res.status(200).json({
+        status: 'success',
+        results:tours.length,
+        data: {
+            tours
+        }
+    })
+});
+
+/*
+* Get a Signle Tour
+* **********************************************/
+app.get('/api/v1/tours/:id', (req,res)=>{
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
     
-    console.log(res);
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        });
+    }
+    
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
 });
 
-app.post('/', (req,res) => {
-    res.send('You can post to this endpoint....');
+app.post('/api/v1/tours', (req, res) => {
+    //console.log(req.body);
+    const newId = tours[tours.length - 1].id +1;
+    const newTour = Object.assign({id:newId}, req.body);
+    
+    tours.push(newTour);
+    
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status:'Success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+    // res.send(`Done`);
 });
 
+
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`App runnig on port ${PORT}...`);
 });
