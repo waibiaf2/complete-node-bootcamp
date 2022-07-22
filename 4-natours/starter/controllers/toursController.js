02b82de630338981795f21444185b7413c341955
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-07-22 21:34:08 +0300

    adds function to write tour to file

--- 4-natours/starter/controllers/toursController.js
+++ 4-natours/starter/controllers/toursController.js
@@ -39,14 +39,16 @@ exports.getTour = (req, res) => {
 exports.postTour = (req,res) => {
     const newId = tours[tours.length -1].id + 1;
     const newTour = Object.assign({id: newId}, req.body);
+    console.log(newTour);
     
     tours.push(newTour);
     
-    fs.writeFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, )
-    res.status(200).json({
-        status: 'Success',
-        data: {
-            newTour
-        }
+    fs.writeFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(200).json({
+            status: 'Success',
+            data: {
+                tour: newTour
+            }
+        })
     })
 }
