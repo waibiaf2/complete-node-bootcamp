481bf8a95c87de11d2340a577ae20284d0b98bff
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-07-28 02:29:08 +0300

    adds config.env file, and dotenv package

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,9 +8,12 @@ const userRoute = require('./routes/usersRoutes');
 
 
 
-/*Middleware
+/*
+Middleware
 ***********************************************************/
-app.use(morgan('dev'));
+if (NODE_ENV = 'development') {
+    app.use(morgan('dev'));
+}
 
 app.use('/api/v1/tours', toursRoute);
 app.use('/api/v1/users', userRoute);
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
+const config_env = require('./config.env')
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`Server is started and running on port, ${PORT}`);
 })
