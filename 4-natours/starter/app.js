f8f84a4f4e99ab7070603e5d6858d2cd4f06539d
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-07-28 02:08:38 +0300

    adds config.env file, dotenv package

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,13 +4,16 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-const app = express()
+const app = express();
+
+
 
 /*
 Middleware
 ***********************************************/
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req,res,next)=>{
     console.log(`Hello from the middleware👋👋👋👋👋👋👋👋`);
