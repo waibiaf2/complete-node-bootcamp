cc762335a72a505a9a551e85a234682fd970ee55
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-07-29 17:35:25 +0300

    updates createTour method

--- 4-natours/starter/controllers/toursController.js
+++ 4-natours/starter/controllers/toursController.js
@@ -37,21 +37,21 @@ exports.getTour = async (req, res) => {
  
 }
 
-exports.createTour = (req,res) => {
-    // const newId = tours[tours.length -1].id + 1;
-    // const newTour = Object.assign({id: newId}, req.body);
-    // console.log(newTour);
-    //
-    // tours.push(newTour);
-    
-    // fs.writeFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    //     res.status(200).json({
-    //         status: 'Success',
-    //         data: {
-    //             tour: newTour
-    //         }
-    //     })
-    // })
+exports.createTour = async (req,res) => {
+   try {
+       const tour = await Tour.create(req.body);
+       res.status(201).json({
+           status: 'success',
+           data: {
+               tour
+           }
+       })
+   } catch (e) {
+       res.status(404).json({
+           status: 'fail',
+           message: e
+       })
+   }
 }
 
 exports.updateTour = (req, res) => {
