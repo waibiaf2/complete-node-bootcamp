5b1f94cc4642c49874a956e63cb9aec9a4f887b4
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-08-03 20:52:41 +0300

    updates gitignore file to add visual studio files

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,7 +1,8 @@
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
-  try {
+    try {
+
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
